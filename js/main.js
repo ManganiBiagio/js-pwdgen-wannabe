bd8dbc0403cbf55c2398d6cd7e3e42d0b2ba874f
ManganiBiagio <biagiomangani@live.it>
2022-10-26 14:24:20 +0200

    layout

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const inputName=document.getElementById("nome");
+const inputCognome=document.getElementById("cognome");
+const inputColore=document.getElementById("colore");
+const outputSpan=document.getElementById("output-span");
+const bottoneForm=document.getElementById("bottone-form")
+console.log(inputName.innerHTML);
+console.log(inputCognome);
+console.log(outputSpan);
