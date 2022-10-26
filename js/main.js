6af3b97e53cd9e5d66e4ea793122da8615415285
ManganiBiagio <biagiomangani@live.it>
2022-10-26 15:13:35 +0200

    button visibility

--- js/main.js
+++ js/main.js
@@ -4,16 +4,22 @@ const inputCognome=document.getElementById("cognome");
 const inputColore=document.getElementById("colore");
 const outputSpan=document.getElementById("output-span");
 const bottoneForm=document.getElementById("bottone-form");
+const boxOutput=document.getElementById("box-output")
+console.log(boxOutput);
 
 
 
 
 bottoneForm.addEventListener("click",()=> GeneratorPsw());
 
+
  function GeneratorPsw(){
     let outputText=inputName.value+inputCognome.value+inputColore.value;
     console.log(outputSpan);
     outputSpan.innerHTML=` ${outputText}21`;
+    boxOutput.style="visibility: visible";
+
+
     
     
 }
