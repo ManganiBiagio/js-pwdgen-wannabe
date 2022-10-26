e26ae5a6ae38be2f84980ef3c472e127e6a2556a
ManganiBiagio <biagiomangani@live.it>
2022-10-26 15:24:38 +0200

    easter egg 3 execution

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ const inputColore=document.getElementById("colore");
 const outputSpan=document.getElementById("output-span");
 const bottoneForm=document.getElementById("bottone-form");
 const boxOutput=document.getElementById("box-output");
-
+let counter=0;
 
 
 
@@ -13,9 +13,14 @@ bottoneForm.addEventListener("click",()=> GeneratorPsw());
 
 
  function GeneratorPsw(){
+    
     let outputText=inputName.value+inputCognome.value+inputColore.value;
     outputSpan.innerHTML=` ${outputText}21`;
     boxOutput.style="visibility: visible";
+    counter++;
+    if(counter>2){
+        alert("Accidenti hai parecchie cose dal tenere al sicuro");
+    }
 
 
     
