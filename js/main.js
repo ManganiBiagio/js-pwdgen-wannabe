6edb8f25f8e4adeed1327ee2841f5cd5200adbdb
ManganiBiagio <biagiomangani@live.it>
2022-10-26 15:02:43 +0200

    function genPsw

--- js/main.js
+++ js/main.js
@@ -1,8 +1,19 @@
+
 const inputName=document.getElementById("nome");
 const inputCognome=document.getElementById("cognome");
 const inputColore=document.getElementById("colore");
 const outputSpan=document.getElementById("output-span");
-const bottoneForm=document.getElementById("bottone-form")
-console.log(inputName.innerHTML);
-console.log(inputCognome);
-console.log(outputSpan);
+const bottoneForm=document.getElementById("bottone-form");
+
+
+
+
+bottoneForm.addEventListener("click",()=> GeneratorPsw());
+
+ function GeneratorPsw(){
+    let outputText=inputName.value+inputCognome.value+inputColore.value;
+    console.log(outputSpan);
+    outputSpan.innerHTML=` ${outputText}`;
+    
+    
+}
